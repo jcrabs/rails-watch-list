a9faa836b169111bf9c14f9c7f81a497b9376f1f
Julia Krebs <152524036+jcrabs@users.noreply.github.com>
2024-08-15 15:16:08 +0200

    new bookmark form now displays in the list show

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, :list_id, presence: true
+  validates :comment, :movie_id, :list_id, presence: true
   validates_uniqueness_of :movie_id, :scope => [:list_id]
   validates :comment, length: { minimum: 6 }
 end
